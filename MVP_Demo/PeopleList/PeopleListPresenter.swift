61196c8ad551ea9a1540986d96295d88d6549c20
Shea <sclark.tieche@gmail.com>
2016-09-01 16:32:17 -0700

    basic MVP implementation w/out tests

--- MVP_Demo/PeopleList/PeopleListPresenter.swift
+++ MVP_Demo/PeopleList/PeopleListPresenter.swift
@@ -1,5 +1,23 @@
 import Foundation
 
-protocol PeopleListPresenter {
+protocol PeopleListPresenterType {
+    init(view: PeopleListView, peopleService: PeopleServiceType)
+    func getUsers()
+}
 
+class PeopleListPresenter : PeopleListPresenterType {
+
+    private let peopleService: PeopleServiceType
+    private let view: PeopleListView
+
+    required init(view: PeopleListView, peopleService: PeopleServiceType) {
+        self.peopleService = peopleService
+        self.view = view
+    }
+
+    func getUsers() {
+        peopleService.getPeople { [weak self] people in
+            self?.view.set(people: people)
+        }
+    }
 }
--- MVP_Demo/PeopleList/PeopleListViewController.swift
+++ MVP_Demo/PeopleList/PeopleListViewController.swift
@@ -2,9 +2,44 @@ import UIKit
 
 class PeopleListViewController: UIViewController {
 
-    override func viewDidLoad() {
+    private var peoplePresenter: PeopleListPresenterType!
+    private var people: [Person] = []
 
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        peoplePresenter = PeopleListPresenter(view: self, peopleService: PeopleService())
+        peoplePresenter.getUsers()
+    }
+}
+
+extension PeopleListViewController : PeopleListView {
+
+    func set(people people: [Person]) {
+        self.people = people
+        tableView.reloadData()
     }
 
+}
+
+extension PeopleListViewController : UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "person"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) ?? UITableViewCell(style: .Subtitle, reuseIdentifier: cellIdentifier)
+
+        let person = people[indexPath.row]
+        cell.textLabel?.text = person.name
+        cell.detailTextLabel?.text = person.phone
+
+        return cell
+    }
 }
 
